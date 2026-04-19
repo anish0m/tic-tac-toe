6949fa294fc8c61cbd5e5a5d15bd3b8412b0df31
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2026-04-19 15:21:00 +0600

    feat: add restart functionality to GameOver component

--- src/App.jsx
+++ src/App.jsx
@@ -71,6 +71,10 @@ function App() {
     });
   }
 
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -86,7 +90,9 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
